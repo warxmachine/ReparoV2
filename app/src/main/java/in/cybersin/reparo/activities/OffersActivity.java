88ecb24de5cae80a11f3b98a5661f114cfc6e39f
warxmachine <shubhendu@cybersin.in>
2022-05-25 10:45:29 +0530

    Major Development

---
+++ app/src/main/java/in/cybersin/reparo/activities/OffersActivity.java
@@ -0,0 +1,16 @@
+package in.cybersin.reparo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.cybersin.reparo.R;
+
+public class OffersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offers);
+    }
+}
